20c7a8d78e04dde0b5272bbe3fbeef33528f459f
irishaggiebaron <james.vranish@gmail.com>
2025-11-25 02:31:14 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -4267,58 +4267,31 @@ var ptx_lunr_docs = [
   "body": "pH "
 },
 {
-  "id": "sec-acids-bases4",
+  "id": "sec-acidsbases7",
   "level": "1",
-  "url": "sec-acids-bases4.html",
+  "url": "sec-acidsbases7.html",
   "type": "Section",
   "number": "10.4",
-  "title": "Acids and Bases 4",
-  "body": " Acids and Bases 4    Buffers    Buffers are substances that can be added to water to make the water resistant to changes in pH. A buffer consists of either a weak acid and its conjugate base or a weak base and its conjugate acid. How does this mixture of substances make the solution more resistant to changes in pH?  When a strong acid is added to water, it will form hydronium ions and the concentration of the hydronium ions formed will be the same as the concentration of the acid that was just added, because a strong acid completely dissociates upon addition to water. Likewise if a strong base is added to water, it will form the number of hydroxide ions that are contained in that substance that was added, again because a strong base will completely dissociate when added to water. The additional hydronium ion or hydroxide ion will then directly affect the pH of the solution.   When a buffer is present, the solution contains an additional solute that is an acid and another that is a base. These acids and bases are capable of reacting with any base or acid that might get added to the solution, helping to neutralize it. The picture below might help.  Every buffer has a buffering capacity . This is the amount of additional acid or base that can be added without exceeding the amount of buffering compounds. So if a buffered solution contains 0.1M weak base and 0.2M of its conjugate acid, then the solution could handle the addition of up to 0.1M hydronium ions before running out of weak base (because the weak base will react with the hydronium ions) and up to 0.2M hydroxide ions (because the hydroxide would react with the conjugate acid). Amounts of hydronium above 0.1M would exceed the buffering capacity of the solution as would addition of more than 0.2M hydroxide ion.   Exercise 4:      Which of the following mixtures of chemicals would make good buffers?            \/                      \/             Create a drawing similar to the figure above that shows how a buffer can resist changes to pH caused by adding in strong base.      If a solution contains 0.11 moles of weak base and 0.33 moles of weak acid, then how many moles of nitric acid could be added without exceeding the buffering capacity?      How many moles of sodium hydroxide could be added without exceeding the buffering capacity?      How many moles of calcium hydroxide could be added without exceeding the buffering capacity?        Acid-Base titration review   An acid base titration is simply a solution reaction that is used to measure the concentration of an acid or base. If we begin with a known volume of solution with an unknown concentration of acid, we can determine its concentration by measuring the number of moles of base that we have to add to neutralize it. The easiest way to do that is to slowly add a solution of base of known concentration to the solution using a burette, which allows us to measure the volume of base needed. If we have the volume and concentration of the base, we can use this to determine the number of moles of base. From there, we can convert moles of base to moles of acid using the balanced chemical equation. If we know moles of acid and its volume (remember we need to know this when we start the titration), then we can divide them to determine the concentration.   Exercise 5:      What is the molarity of an HCl solution if 7.50 mL of the solution was neutralized by the addition of 14.30mL of 0.0980 M NaOH?      What was the starting pH of the HCl solution?        What is the molarity of a NaOH solution if 1.5 L of the solution was neutralized by the addition of 15.33mL of 6.00M HCl?      What was the starting pH of that NaOH solution?        What is the molarity of a magnesium hydroxide solution if 20.00 mL of it was neutralized by the addition of 4.75 mL of 0.100M HCl?     Key:   Exercise 4:      Which of the following mixtures of chemicals would make good buffers?                                           Drawings may vary      If a solution contains 0.11 moles of weak base and 0.33 moles of weak acid, then how many moles of nitric acid could be added without exceeding the buffering capacity? 0.11 moles      How many moles of sodium hydroxide could be added without exceeding the buffering capacity? 0.33 moles      How many moles of calcium hydroxide could be added without exceeding the buffering capacity? 0.17 moles        Exercise 5:      What is the molarity of an HCl solution if 7.50 mL of the solution was neutralized by the addition of 14.30mL of 0.0980 M NaOH?      What was the starting pH of the HCl solution?        What is the molarity of a NaOH solution if 1.5 L of the solution was neutralized by the addition of 15.33mL of 6.00M HCl?      What was the starting pH of that NaOH solution?        What is the molarity of a magnesium hydroxide solution if 20.00 mL of it was neutralized by the addition of 4.75 mL of 0.100M HCl?     "
+  "title": "pH",
+  "body": " pH   Summary:     pH is a different way of expressing the concentration of hydronium ion in a solution    Neutral pH is 7. Acidic pH is less than 7. Basic pH is greater than 7.    Calculations involving pH involve logarithms\/power functions     pH   When scientists describe how acidic or basic a solution is, we could report the concentration of hydronium ion or hydroxide ion. However, because the numbers are so small and usually written in scientific notation, this becomes quite cumbersome. To solve this problem, we instead often transform the concentration of hydronium ion into a pH value using the formula below:     This version of the equation can be used to find the pH value from a concentration of hydronium ion (which MUST be molarity). We can figure out the pH of a neutral solution as shown:      pH values lower than 7 are said to be acidic solutions. pH values above 7 are basic solutions. The reason why will be determined as a part of your exercises.  In some cases, we’ll need to figure out the concentration of hydronium ion from a pH value. This requires rearrangement of the pH equation using algebra skills. The tricky part of this is that we need to ‘undo’ the log function. This is done by taking 10 and raising it to the power of everything on that side of the equation as shown below:              "
 },
 {
-  "id": "sec-acids-bases4-2",
+  "id": "sec-acidsbases7-2",
   "level": "2",
-  "url": "sec-acids-bases4.html#sec-acids-bases4-2",
+  "url": "sec-acidsbases7.html#sec-acidsbases7-2",
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
-  "body": "Buffers "
+  "body": "Summary: "
 },
 {
-  "id": "sec-acids-bases4-3",
+  "id": "sec-acidsbases7-4",
   "level": "2",
-  "url": "sec-acids-bases4.html#sec-acids-bases4-3",
+  "url": "sec-acidsbases7.html#sec-acidsbases7-4",
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
-  "body": "Buffers "
-},
-{
-  "id": "sec-acids-bases4-6",
-  "level": "2",
-  "url": "sec-acids-bases4.html#sec-acids-bases4-6",
-  "type": "Paragraph (with a defined term)",
-  "number": "",
-  "title": "",
-  "body": "buffering capacity "
-},
-{
-  "id": "sec-acids-bases4-9",
-  "level": "2",
-  "url": "sec-acids-bases4.html#sec-acids-bases4-9",
-  "type": "Paragraph (with a defined term)",
-  "number": "",
-  "title": "",
-  "body": "Acid-Base titration review "
-},
-{
-  "id": "sec-acids-bases4-10",
-  "level": "2",
-  "url": "sec-acids-bases4.html#sec-acids-bases4-10",
-  "type": "Paragraph (with a defined term)",
-  "number": "",
-  "title": "",
-  "body": "titration "
+  "body": "pH "
 },
 {
   "id": "sec-acidsbases8",
